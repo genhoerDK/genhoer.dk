9c971eddb774cdcc3a7751802e8b0326ceaea246
Genhoer <te@genhoer.dk>
2025-01-19 18:47:45 +0100

    Test navigation

---
+++ src/app/om.js/page.js
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+      <div>
+        <h1>Om os</h1>
+        <p>Velkommen til om-siden.</p>
+      </div>
+    );
+  }
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,20 @@
+'use client';
 
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleNavigation = () => {
+    router.push('/om');
+  };
+
   return (
-    <div className='size-10 bg-slate-200'></div>
+    <div
+      className='size-10 bg-slate-200 cursor-pointer'
+      onClick={handleNavigation}
+    >
+      Klik her for at gå til Om-siden
+    </div>
   );
 }
