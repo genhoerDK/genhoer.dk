d13c178a9a4b9509c312a89936b13b94ffb9b212
ThomasEgMatthiesen <kontakt@thomaseg.dk>
2025-06-29 11:28:42 +0200

    feat: Update project locations and video links for improved accuracy and accessibility

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import { projects } from '@/data/projects';
 
 export default function Home() {
   return (
-    <ul className="lg:grid grid-cols-3 gap-x-4 px-4 pt-28">
+    <ul className="lg:grid grid-cols-3 gap-x-4 px-2 md:px-4 pt-28">
       {projects.map((project) => (
         <ProjectCard key={`${project.slug}-project-card`} project={project} />
       ))}
