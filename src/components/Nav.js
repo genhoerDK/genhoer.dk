dabe67e824d1b948bd757472092754cae020a181
ThomasEgMatthiesen <kontakt@thomaseg.dk>
2025-07-18 14:57:44 +0200

    feat: Refactor Nav component to use dynamic navigation links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,19 @@
 'use client';
-
 import Link from 'next/link';
 
 export default function Nav() {
 
-  return (
-    <nav className="fixed bottom-0 w-full h-10 md:h-12 flex items-center gap-2 md:gap-4 px-2 md:px-4 bg-zinc-50 z-10">
-        <Link href="" className="text-xl md:text-2xl font-light">Deltag</Link>
-        <Link href="" className="text-xl md:text-2xl font-light">Info</Link>
-    </nav>
-  );
+    const navLinks = [
+        { label: 'Projekter', slug: '' },
+        { label: 'Deltag', slug: '' },
+        { label: 'Info', slug: '' }
+    ];
+
+    return (
+        <nav className="fixed bottom-0 w-full h-10 md:h-12 flex items-center gap-4 md:gap-6 px-2 md:px-4 bg-zinc-50 z-10">
+            {navLinks.map(({ label, slug }) => (
+                <Link key={`nav-link-${label}`} href={slug} className="text-lg md:text-xl font-light">{label}</Link>
+            ))}
+        </nav>
+    );
 };
