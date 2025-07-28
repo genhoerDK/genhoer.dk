14616c29448f61e960c1bf339d69882ed1f314da
ThomasEgMatthiesen <kontakt@thomaseg.dk>
2025-07-28 10:18:12 +0200

    fix: Enhance map interaction by adding project markers with hover effects and project details display

--- src/components/MapLarge.js
+++ src/components/MapLarge.js
@@ -1,11 +1,15 @@
 "use client";
 
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { formatDates, formatPartners } from '@/utilities/formatters';
 import * as d3 from "d3";
 import * as topojson from "topojson-client";
 
 export default function MapLarge({ projects }) {
     const svgRef = useRef(null);
+    const router = useRouter();
+    const [activeProject, setActiveProject] = useState(null);
 
     useEffect(() => {
         let geoData = null;
@@ -51,47 +55,68 @@ export default function MapLarge({ projects }) {
             const zoomScale = 4;
 
             // Draw the markers
-            projects.forEach((project) => {
-                const [lon, lat] = project.coordinates;
-                const [x, y] = projection([lon, lat]);
+            const markers = g.selectAll(".marker")
+                .data(projects)
+                .enter()
+                .append("circle")
+                .attr("class", "marker")
+                .attr("cx", d => projection(d.coordinates)[0])
+                .attr("cy", d => projection(d.coordinates)[1])
+                .attr("r", 6)
+                .attr("fill", "#71717A")
+                .attr("stroke", "#FAFAFA")
+                .attr("stroke-width", 2)
+                .style("cursor", "pointer")
+                .on("mouseenter", function (event, d) {
+                    const [x, y] = projection(d.coordinates);
+                    const [mouseX, mouseY] = d3.pointer(event);
+                    const translateX = mouseX - x * zoomScale;
+                    const translateY = mouseY - y * zoomScale;
 
-                g.append("circle")
-                    .attr("cx", x)
-                    .attr("cy", y)
-                    .attr("r", 6)
-                    .attr("fill", "#52525b")
-                    .attr("stroke", "#fff")
-                    .attr("stroke-width", 2)
-                    .style("cursor", "pointer")
-                    .on("mouseenter", function (event) {
-                        const [mouseX, mouseY] = d3.pointer(event);
+                    setActiveProject(d);
 
-                        const translateX = mouseX - x * zoomScale;
-                        const translateY = mouseY - y * zoomScale;
+                    g.transition()
+                        .delay(200)
+                        .duration(500)
+                        .attr("transform", `translate(${translateX},${translateY}) scale(${zoomScale})`);
 
-                        g.transition()
-                            .duration(600)
-                            .attr("transform", `translate(${translateX},${translateY}) scale(${zoomScale})`);
+                    g.selectAll("path")
+                        .transition()
+                        .delay(200)
+                        .duration(500)
+                        .attr("stroke-opacity", p => p.properties.KOMKODE === d.komkode ? 1 : 0.1)
+                        .attr("fill-opacity", p => p.properties.KOMKODE === d.komkode ? 1 : 0);
 
-                        g.selectAll("path")
-                            .transition()
-                            .duration(600)
-                            .attr("stroke-opacity", d => d.properties.KOMKODE === project.komkode ? 1 : 0.1)
-                            .attr("fill-opacity", d => d.properties.KOMKODE === project.komkode ? 1 : 0);
-                    })
-                    .on("mouseleave", function () {
-                        g.transition()
-                            .duration(600)
-                            .attr("transform", `translate(0,0) scale(1)`);
+                    markers.transition()
+                        .delay(200)
+                        .duration(500)
+                        .attr("r", r => r === d ? 4 : 0)
+                        .attr("fill", "#27272A")
+                        .attr("stroke-width", 1);
 
-                        g.selectAll("path")
-                            .transition()
-                            .duration(600)
-                            .style("opacity", 1)
-                            .attr("stroke-opacity", 1)
-                            .attr("fill-opacity", 1);
-                    });
-            });
+                })
+                .on("mouseleave", function () {
+                    g.transition()
+                        .duration(500)
+                        .attr("transform", `translate(0,0) scale(1)`);
+
+                    g.selectAll("path")
+                        .transition()
+                        .duration(500)
+                        .attr("stroke-opacity", 1)
+                        .attr("fill-opacity", 1);
+
+                    markers.transition()
+                        .duration(500)
+                        .attr("r", 6)
+                        .attr("fill", "#71717A")
+                        .attr("stroke-width", 2);
+
+                    setActiveProject(null);
+                })
+                .on("click", (event, d) => {
+                    router.push(`/${d.slug}`);
+                });
         };
 
         d3.json("/map_low.json").then((topology) => {
@@ -105,7 +130,28 @@ export default function MapLarge({ projects }) {
 
     return (
         <div className="relative size-full">
+            <div className={`absolute inset-0 -z-10 bg-cover bg-center bg-no-repeat transition-opacity delay-200 duration-500 ${activeProject ? "opacity-20" : "opacity-0"}`} style={{ backgroundImage: activeProject ? `url(${activeProject.coverImage})` : undefined, }} />
             <svg ref={svgRef} className="size-full" />
+
+            {activeProject && (
+                <div className="absolute inset-0 z-10 flex items-center justify-center px-4 pb-4 pointer-events-none transition-opacity duration-300">
+                    <div className="bg-zinc-900/80 text-white p-4 max-w-md w-full">
+                        <h2 className="font-light uppercase text-md leading-none mb-2">
+                            {activeProject.title}
+                        </h2>
+                        {[
+                            { label: "Udstilling", info: formatDates(activeProject.startDate, activeProject.endDate) },
+                            { label: "Lokalitet", info: activeProject.location },
+                            { label: "Partnere", info: formatPartners(activeProject.partners) },
+                        ].map(({ label, info }) => (
+                            <div key={`${activeProject.title}-card-info-${label}`} className="grid grid-cols-6 gap-x-2 pb-2 pointer-events-none">
+                                <p className="col-span-1 text-[0.5rem] uppercase leading-none mt-[5px]">{label}</p>
+                                <p className="col-start-2 col-span-full font-light text-xs truncate">{info}</p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
