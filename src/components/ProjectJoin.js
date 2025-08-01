75255392ad11e0bd189244514e301d57dae3a0d8
ThomasEgMatthiesen <kontakt@thomaseg.dk>
2025-08-01 12:09:43 +0200

    Adjust padding and font size in ProjectJoin component for improved readability

--- src/components/ProjectJoin.js
+++ src/components/ProjectJoin.js
@@ -17,9 +17,9 @@ export default function ProjectJoin({ slug, show }) {
                 <div className="min-h-dvh flex flex-col justify-center pointer-events-none px-2 md:px-4">
                     <div className="grid grid-cols-12 xl:grid-cols-16 gap-x-4">
                         <div className="col-span-10 sm:col-span-8 sm:col-start-2 md:col-span-8 md:col-start-3 xl:col-span-6 xl:col-start-4">
-                            <h2 className="md:text-xl uppercase font-light pb-4">Vær med</h2>
+                            <h2 className="md:text-xl uppercase font-light pb-2">Vær med</h2>
                             {workshop.map((string, i) => (
-                                <p className="font-light pb-4 text-sm md:text-base" key={`${slug}-workshop-${i}`}>
+                                <p className="font-light pb-4 text-xs md:text-sm" key={`${slug}-workshop-${i}`}>
                                     {string}
                                 </p>
                             ))}
